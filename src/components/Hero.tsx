e872c3fa1e19638810edd2a8bee2056f0609277f
Sebastian Widin <contact@sebastianwidin.se>
2023-04-30 16:32:23 +0200

    delivery section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,29 +3,36 @@ import Image from 'next/image'
 
 import styles from '@/styles/Hero.module.scss'
 
-const Hero = ({ heroContent, heroImage }: any) => {
+const Hero = ({ deliveryContent, heroContent, heroImage }: any) => {
 	console.log(heroContent, heroImage)
 	return (
-		<section className={styles.hero_section}>
-			<div className={styles.hero_content}>
-				<h1>{heroContent.title}</h1>
-				<p dangerouslySetInnerHTML={{ __html: heroContent.body }}></p>
-			</div>
-			{heroImage.src && heroImage.alt ? (
-				<div className={styles.hero_image}>
-					<Image
-						src={heroImage.src}
-						alt={heroImage.alt}
-						fill
-						style={{ objectFit: 'cover' }}
-						sizes='(min-width: 60em) 24vw,
+		<>
+			<section className={styles.hero_section}>
+				<div className={styles.delivery_content}>
+					<h2>{deliveryContent.title}</h2>
+					<p dangerouslySetInnerHTML={{ __html: deliveryContent.body }}></p>
+					<input type='number' placeholder='Fyll i ditt postnummer' />
+				</div>
+				{heroImage.src && heroImage.alt ? (
+					<div className={styles.hero_image}>
+						<Image
+							src={heroImage.src}
+							alt={heroImage.alt}
+							fill
+							style={{ objectFit: 'cover' }}
+							sizes='(min-width: 60em) 24vw,
 					(min-width: 28em) 45vw,
 					100vw'
-						priority
-					/>
-				</div>
-			) : null}
-		</section>
+							priority
+						/>
+					</div>
+				) : null}
+			</section>
+			<section className={styles.hero_content}>
+				<h1>{heroContent.title}</h1>
+				<p dangerouslySetInnerHTML={{ __html: heroContent.body }}></p>
+			</section>
+		</>
 	)
 }
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,7 +10,7 @@ const App = ({ Component, pageProps }: AppProps) => {
 			<Head>
 				<meta
 					name='viewport'
-					content='width=device-width, initial-scale=1.0, viewport-fit=cover'
+					content='width=device-width, initial-scale=1.0, maximum-scale=1", viewport-fit=cover'
 				/>
 				<link rel='shortcut icon' href='favicons/favicon.ico' />
 				<link
