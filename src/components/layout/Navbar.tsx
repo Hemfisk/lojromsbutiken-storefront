fdc6a332652dbfb721b147674312cb5fc942f352
Sebastian Widin <contact@sebastianwidin.se>
2023-05-15 23:36:15 +0200

    mobile navigation

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { NextFont } from 'next/dist/compiled/@next/font'
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined'
+import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined'
 
-import styles from '@/styles/Navbar.module.scss'
+import styles from '@/styles/Navigation.module.scss'
 
 interface Props {
 	fontFamily: NextFont
@@ -42,57 +44,104 @@ const NavLink = ({
 
 const Navbar = ({ fontFamily, navigation, currentPage }: Props) => {
 	const [active, setActive] = useState(currentPage || '')
+	const [mobileNavigationOpen, setMobileNavigationOpen] = useState(false)
+
+	const router = useRouter()
+
+	useEffect(() => {
+		router.events.on('routeChangeComplete', () => {
+			setMobileNavigationOpen(false)
+		})
+	}, [router.events])
 
 	useEffect(() => {
 		setActive(currentPage)
 	}, [currentPage])
 
-	return (
-		<div className={`${styles.navigation_container} ${fontFamily.className}`}>
-			<div className={styles.navigation_content}>
-				<div className={styles.mobile_menu_container}>
+	const mobileNavigation = () => {
+		console.log(mobileNavigationOpen)
+		if (!mobileNavigationOpen) {
+			return null
+		}
+
+		return (
+			<>
+				<div className={`${styles.mobile_navigation}  ${fontFamily.className}`}>
 					<div
 						tabIndex={0}
-						onClick={() => console.log('mobile menu click')}
+						onClick={() => setMobileNavigationOpen((prevState) => !prevState)}
 						className={styles.mobile_menu_button}
 					>
-						<MenuOutlinedIcon />
+						<CloseOutlinedIcon />
+					</div>
+					<div className={styles.navigation}>
+						{navigation.map((link) => (
+							<NavLink
+								key={`${link.handle}_mobile`}
+								title={link.title}
+								handle={link.handle}
+								active={active === link.handle}
+							/>
+						))}
 					</div>
 				</div>
-				<div className={styles.logo_container}>
-					<Link href='/' title='Dalafisk.se'>
-						<Image
-							src='/LOGO_dalafisk.png'
-							alt='Dalafisk logotyp'
-							fill
-							sizes='(min-width: 60em) 24vw,
+				<div
+					className={styles.mobile_navigation_blur}
+					onClick={() => setMobileNavigationOpen((prevState) => !prevState)}
+				/>
+			</>
+		)
+	}
+
+	return (
+		<>
+			{mobileNavigation()}
+			<div className={`${styles.navigation_container} ${fontFamily.className}`}>
+				<div className={styles.navigation_content}>
+					<div className={styles.mobile_menu_container}>
+						<div
+							tabIndex={0}
+							onClick={() => setMobileNavigationOpen((prevState) => !prevState)}
+							className={styles.mobile_menu_button}
+						>
+							<MenuOutlinedIcon />
+						</div>
+					</div>
+					<div className={styles.logo_container}>
+						<Link href='/' title='Dalafisk.se'>
+							<Image
+								src='/LOGO_dalafisk.png'
+								alt='Dalafisk logotyp'
+								fill
+								sizes='(min-width: 60em) 24vw,
                     (min-width: 28em) 45vw,
                     100vw'
-							priority
-						/>
-					</Link>
-				</div>
-				<nav className={styles.navigation}>
-					{navigation.map((link) => (
-						<NavLink
-							key={link.handle}
-							title={link.title}
-							handle={link.handle}
-							active={active === link.handle}
-						/>
-					))}
-				</nav>
-				<div className={styles.cart_container}>
-					<div
-						tabIndex={0}
-						onClick={() => console.log('cart click')}
-						className={styles.cart_button}
-					>
-						<ShoppingCartOutlinedIcon />
+								priority
+							/>
+						</Link>
+					</div>
+					<nav className={styles.navigation}>
+						{navigation.map((link) => (
+							<NavLink
+								key={link.handle}
+								title={link.title}
+								handle={link.handle}
+								active={active === link.handle}
+							/>
+						))}
+					</nav>
+					<div className={styles.cart_container}>
+						<div
+							tabIndex={0}
+							onClick={() => console.log('cart click')}
+							className={styles.cart_button}
+						>
+							<ShoppingCartOutlinedIcon />
+						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	)
 }
 
